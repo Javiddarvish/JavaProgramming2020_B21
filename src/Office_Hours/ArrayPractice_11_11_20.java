da278c1348da0ff7ad531cdf26fb60e4b6de7285
Javiddarvish <javid7darvish@gmail.com>
2020-11-11 15:24:36 -0500

    Java

--- src/Office_Hours/ArrayPractice_11_11_20.java
+++ src/Office_Hours/ArrayPractice_11_11_20.java
@@ -3,26 +3,31 @@ package Office_Hours;
 public class ArrayPractice_11_11_20 {
     public static void main(String[] args) {
 
-        int[] arr1 ={1,2,3,4,5};
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        int[] arr2 = {4,5,6,7,8};
+        int[] arr2 = {10, 11, 12, 13, 14, 15, 1};
 
-        int element = arr1[0];
-        for (int i = 0; i <= arr2.length-1;i++){
-            if (element == arr2[i]){
-                System.out.println(element);
+        for (int j = 0; j <= arr1.length - 1; j++) {
+
+
+            int element = arr1[j];
+            for (int i = 0; i <= arr2.length - 1; i++) {
+                if (element == arr2[i]) {
+                    System.out.println(element);
+                    break;
+                }
+            }
+            }
+        System.out.println("========================================");
+
+        for (int element : arr1) {
+
+            for (int each : arr2) {
+                if (each == element) {
+                    System.out.println(element);
+                }
             }
         }
-
-
-
-
-
-
-
-
-
-
 
 
 
